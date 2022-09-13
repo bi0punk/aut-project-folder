cce49567ab2453f4ab0e583549c0a5d82bf98d91
sysbot <systecinfo_cl@criptext.com>
2022-09-13 16:24:23 -0300

    real

--- app.py
+++ app.py
@@ -8,30 +8,26 @@ menu = ConsoleMenu("Automatic Project Folder Creator", "Automatic creation of pr
 # Create some items
 # Crear algunos elementos
 
-# MenuItem is the base class for all items, it doesn't do anything when selected
+
 # MenuItem es la clase base para todos los elementos, no hace nada cuando se selecciona
 menu_item = MenuItem("Menu Item")
 
-# A FunctionItem runs a Python function when selected
+
 # Un elemento de función ejecuta una función de Python cuando se selecciona
 function_item = FunctionItem("Call a Python function", input, ["Enter an input"])
 
-# A CommandItem runs a console command
+
 # Un CommandItem ejecuta un comando de consola
 command_item = CommandItem("Run a console command",  "touch hello.txt")
 
-# A SelectionMenu constructs a menu from a list of strings
+
 # Un SelectionMenu construye un menú a partir de una lista de cadenas
 selection_menu = SelectionMenu(["item1", "item2", "item3"])
 
-# A SubmenuItem lets you add a menu (the selection_menu above, for example)
-# as a submenu of another menu
 
 # Un SubmenuItem le permite agregar un menú (el menú de selección anterior, por ejemplo)
 # como submenú de otro menú
 submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
-
-# Once we're done creating them, we just add the items to the menu
 # Una vez que hayamos terminado de crearlos, solo agregamos los elementos al menú
 menu.append_item(menu_item)
 menu.append_item(function_item)

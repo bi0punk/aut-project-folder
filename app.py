56c0b65344b5f055f801b799112556115cd2ef0d
sysbot <systecinfo_cl@criptext.com>
2022-09-13 15:50:18 -0300

    real

--- app.py
+++ app.py
@@ -1,54 +1,43 @@
-def mostrar_menu(opciones):
-    print('Seleccione una opción:')
-    for clave in sorted(opciones):
-        print(f' {clave}) {opciones[clave][0]}')
+from consolemenu import *
+from consolemenu.items import *
 
+# Create the menu
+# Crea el menú
+menu = ConsoleMenu("Automatic Project Folder Creator", "Automatic creation of project structure dir")
 
-def leer_opcion(opciones):
-    while (a := input('Opción: ')) not in opciones:
-        print('Opción incorrecta, vuelva a intentarlo.')
-    return a
+# Create some items
+# Crear algunos elementos
 
+# MenuItem is the base class for all items, it doesn't do anything when selected
+# MenuItem es la clase base para todos los elementos, no hace nada cuando se selecciona
+menu_item = MenuItem("Menu Item")
 
-def ejecutar_opcion(opcion, opciones):
-    opciones[opcion][1]()
+# A FunctionItem runs a Python function when selected
+# Un elemento de función ejecuta una función de Python cuando se selecciona
+function_item = FunctionItem("Call a Python function", input, ["Enter an input"])
 
+# A CommandItem runs a console command
+# Un CommandItem ejecuta un comando de consola
+command_item = CommandItem("Run a console command",  "touch hello.txt")
 
-def generar_menu(opciones, opcion_salida):
-    opcion = None
-    while opcion != opcion_salida:
-        mostrar_menu(opciones)
-        opcion = leer_opcion(opciones)
-        ejecutar_opcion(opcion, opciones)
-        print()
+# A SelectionMenu constructs a menu from a list of strings
+# Un SelectionMenu construye un menú a partir de una lista de cadenas
+selection_menu = SelectionMenu(["item1", "item2", "item3"])
 
+# A SubmenuItem lets you add a menu (the selection_menu above, for example)
+# as a submenu of another menu
 
-def menu_principal():
-    opciones = {
-        '1': ('Opción 1', accion1),
-        '2': ('Opción 2', accion2),
-        '3': ('Opción 3', accion3),
-        '4': ('Salir', salir)
-    }
+# Un SubmenuItem le permite agregar un menú (el menú de selección anterior, por ejemplo)
+# como submenú de otro menú
+submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
 
-    generar_menu(opciones, '4')
+# Once we're done creating them, we just add the items to the menu
+# Una vez que hayamos terminado de crearlos, solo agregamos los elementos al menú
+menu.append_item(menu_item)
+menu.append_item(function_item)
+menu.append_item(command_item)
+menu.append_item(submenu_item)
 
-
-def accion1():
-    print('Has elegido la opción 1')
-
-
-def accion2():
-    print('Has elegido la opción 2')
-
-
-def accion3():
-    print('Has elegido la opción 3')
-
-
-def salir():
-    print('Saliendo')
-
-
-if __name__ == '__main__':
-    menu_principal()
+# Finally, we call show to show the menu and allow the user to interact
+# Finalmente, llamamos show para mostrar el menú y permitir que el usuario interactúe
+menu.show()

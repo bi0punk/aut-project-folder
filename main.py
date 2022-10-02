88ca41531e4f6f42ad404e707ce6196831842647
sysbot <systecinfo_cl@criptext.com>
2022-10-01 23:55:41 -0300

    2355

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 import os
 from prettytable import PrettyTable
+from colorama import Fore, Back, Style
+
+
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+
+
+print(bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+print(f"{bcolors.WARNING}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
+
+
+
+
+
+
+
 
 x = PrettyTable()
 def menu():
     print("\t\nSelecciona una opcion\n")
     print("\t#############################################################################################")
     wd = os.getcwd()
-    print("\t# Entorno de trabajo ", wd)
+    print(Fore.GREEN + "\t# Entorno de trabajo ")
     print("\t#")
 
     print("\t#")
